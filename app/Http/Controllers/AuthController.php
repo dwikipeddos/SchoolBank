f29e43fe0c10c816f590d3e43573b8a8df182f4a
dwikipeddos <arisxxx5@gmail.com>
2022-12-07 15:04:48 +0800

    auth using bycrpt

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,21 +6,26 @@ use App\Http\Requests\AuthRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function auth(AuthRequest $request)
     {
         $request->ensureNotRateLimited();
-        $user = User::where('email', $request->email)->where('password', $request->password)->first();
+        $user = User::where('email', $request->email)->first();
         if ($user) {
-            $token = $user->createToken($request->userAgent() . ', ip:' . $request->ip())->plainTextToken;
-            return response([
-                'user' => $user,
-                'token' => $token,
-            ]);
+            if (Hash::check($request->password, $user->password)) {
+                $token = $user->createToken($request->userAgent() . ', ip:' . $request->ip())->plainTextToken;
+                return response([
+                    'user' => $user,
+                    'token' => $token,
+                ]);
+            } else {
+                return response(['message' => 'wrong password'], Response::HTTP_FORBIDDEN);
+            }
         } else {
-            return response(['message' => 'wrong email or password'], Response::HTTP_FORBIDDEN);
+            return response(['message' => 'user with specified email doesn\'t exists'], Response::HTTP_FORBIDDEN);
         }
     }
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,11 @@ namespace App\Models;
 
 use Bavix\Wallet\Interfaces\Wallet;
 use Bavix\Wallet\Traits\HasWallet;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements Wallet
@@ -44,6 +46,12 @@ class User extends Authenticatable implements Wallet
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    //mutators and accessors
+    public function password(): Attribute
+    {
+        return Attribute::make(set: fn ($val) => Hash::make($val));
+    }
 
     //relations
     public function student()
