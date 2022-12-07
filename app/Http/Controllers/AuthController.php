197f55586f261aff6b2bf2eafe6e48f1791c74b9
dwikipeddos <arisxxx5@gmail.com>
2022-12-07 12:00:28 +0800

    added auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\AuthRequest;
+use App\Models\User;
+use Illuminate\Http\Response;
 
 class AuthController extends Controller
 {
-    public function auth(Request $request)
+    public function auth(AuthRequest $request)
     {
+        $request->ensureNotRateLimited();
+        $user = User::where('email', $request->email)->where('password', $request->password)->first();
+        if ($user) {
+            $token = $user->createToken($request->userAgent() . ', ip:' . $request->ip())->plainTextToken;
+            return response([
+                'user' => $user,
+                'token' => $token,
+            ]);
+        } else {
+            return response(['message' => 'wrong email or password'], Response::HTTP_FORBIDDEN);
+        }
     }
 }
