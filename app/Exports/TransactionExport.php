777f92f30691a3a425ade23c72b6aa483934c83e
dwikipeddos <arisxxx5@gmail.com>
2022-12-28 09:56:29 +0800

    asd

--- app/Exports/TransactionExport.php
+++ app/Exports/TransactionExport.php
@@ -17,5 +17,12 @@ class TransactionExport implements FromCollection
             ->when($this->dates['start_date'], fn ($builder, $val) => $builder->whereDate('created_at', '<=', $val))
             ->with('payable.student.classroom.school')
             ->get();
+        return $this->mapData($transactions);
+    }
+
+    public function mapData($transactions)
+    {
+        //do stuff
+        return $transactions;
     }
 }
